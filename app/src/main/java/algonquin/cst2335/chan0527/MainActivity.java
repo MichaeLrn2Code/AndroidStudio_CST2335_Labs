aea15d33a3c38eed16d358ac610a07d8be58ef7a
wai <chan0527@algonquinlive.com>
2023-05-17 14:53:21 -0400

    Commit 6: Added function for button to change edit text

--- app/src/main/java/algonquin/cst2335/chan0527/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/chan0527/MainActivity.java
@@ -3,12 +3,32 @@ package algonquin.cst2335.chan0527;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import algonquin.cst2335.chan0527.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding variableBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(variableBinding.getRoot()); //load XML on screen
+
+        TextView mytext = variableBinding.textview;
+        Button btn = variableBinding.mybutton;
+        EditText myedit = variableBinding.myedittext;
+        String editString = myedit.getText().toString();
+        btn.setOnClickListener((View v)->{
+                mytext.setText( "Your edit text has: " + editString);
+        });
     }
+
+
 }
